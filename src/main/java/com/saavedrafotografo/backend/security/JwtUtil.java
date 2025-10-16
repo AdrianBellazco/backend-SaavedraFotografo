9882e16e211c7dc34dc6951fd059e88e1dc1ec77
AdrianBellazco <adrian.saavedra01@uceva.edu.co>
2025-10-15 23:54:13 -0500

    first commit

---
+++ src/main/java/com/saavedrafotografo/backend/security/JwtUtil.java
@@ -0,0 +1,21 @@
+package com.saavedrafotografo.backend.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private static final String SECRET_KEY = "clave_super_secreta_saavedra";
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // 24h
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
